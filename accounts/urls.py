f4ba092e8f99637c8f7e5a7971a92243327e5877
ricardo39985 <ricardo39985@gmail.com>
2022-10-28 13:07:14 -0400

    Rename main to banking

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
-from main import admin
+from banking import admin
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
