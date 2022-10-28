16047a7401c470c44bcca2cc5a7ba31d985f7422
ricardo39985 <ricardo39985@gmail.com>
2022-10-28 10:55:52 -0400

    Change db to postgresql

--- sdr_financial/settings.py
+++ sdr_financial/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'sdr_financial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sdr_financial',
     }
 }
 
