482f9ec714ad0b74f1b958395c1613cd2f16f65c
sammy2342 <sammyfares303@gmail.com>
2022-10-28 15:59:59 -0400

    complete overlay on picture

--- sdr_financial/settings.py
+++ sdr_financial/settings.py
@@ -31,15 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'banking',
-    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts', 'banking'
+    'accounts', 
+    'banking'
 ]
 
 MIDDLEWARE = [
