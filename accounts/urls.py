21356ec27df34c8814d927c2b4ea58151cf366c7
KinglyCode <bayglory@yahoo.com>
2022-10-28 15:00:10 -0700

    fixes admin

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,8 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from . import views
-from banking import admin
 
 urlpatterns = [
-    path('accounts/', include('allauth.urls')),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
 ]
