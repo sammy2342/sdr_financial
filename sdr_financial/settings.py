c45161d45b05dd41a608bc4fed968a07553186d5
ricardo39985 <ricardo39985@gmail.com>
2022-10-29 12:06:23 -0400

    REmove cache

--- sdr_financial/settings.py
+++ sdr_financial/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 from telnetlib import AUTHENTICATION
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,13 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     #Google Auth
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'allauth.socialaccount.providers.google'
-    'accounts', 
+    'allauth.socialaccount.providers.google',
+    'accounts',
     'banking'
 ]
 
