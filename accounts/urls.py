d33211c48ff0819168d834d3754e3a7800b2ed4d
ricardo39985 <ricardo39985@gmail.com>
2022-10-29 12:06:23 -0400

    REmove cache

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,10 @@
 from django.contrib.auth import views as auth_views
-from django.urls import path, include
+from django.urls import path
 from . import views
 from banking import admin
 
 urlpatterns = [
-    path('accounts/', include('allauth.urls')),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
 ]
