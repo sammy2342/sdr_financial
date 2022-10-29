c7ee1787ff7e1d4ed059e36db10a346dfd144b87
Ricardo Persaud <56775968+ricardo39985@users.noreply.github.com>
2022-10-29 12:18:10 -0400

    Update urls.py
    create allauth path

--- sdr_financial/urls.py
+++ sdr_financial/urls.py
@@ -19,6 +19,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('', include('accounts.urls')),
     path('', views.home, name='home'),
 
