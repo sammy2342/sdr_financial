0db7874b2c576a27bec40e36ba0ff5fcc003fc1b
ricardo39985 <ricardo39985@gmail.com>
2022-10-31 13:04:15 -0400

    Add function for dashboard

--- banking/views.py
+++ banking/views.py
@@ -3,6 +3,11 @@ from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+@login_required
 def create_account(request):
-
+    print(request.POST)
     pass
+@login_required
+
+def dashboard(request):
+    return render('dashboard.html')
