2c678f163c93ee111ddb97501597eca70a88d35a
ricardo39985 <ricardo39985@gmail.com>
2022-10-31 13:02:37 -0400

    Add url for dashboard

--- banking/urls.py
+++ banking/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urls = [
-    path('account/create', views.create_account, name='new_bank_account')
+    path('account/create/', views.create_account, name='new_bank_account'),
+    path('dashboard', views.dashboard, name='dashboard')
 ]
