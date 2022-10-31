8554d5e027531a2359f07f29c81d9f5c63960355
ricardo39985 <ricardo39985@gmail.com>
2022-10-31 13:31:27 -0400

    Add path for dashboard

--- banking/urls.py
+++ banking/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 
-urls = [
+urlpatterns = [
     path('account/create/', views.create_account, name='new_bank_account'),
-    path('dashboard', views.dashboard, name='dashboard')
+    path('dashboard/', views.dashboard, name='dashboard')
 ]
