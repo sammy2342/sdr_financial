f09d63d2a4a50390406a744f9fb4aaceb3b63ef4
ricardo39985 <ricardo39985@gmail.com>
2022-10-31 12:58:25 -0400

    import redirect, login_required, add fuction

--- banking/views.py
+++ banking/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+def create_account(request):
+
+    pass
