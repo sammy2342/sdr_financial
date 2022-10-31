0df3a476061f461f6b7a1658324f665a109de356
ricardo39985 <ricardo39985@gmail.com>
2022-10-31 15:19:51 -0400

    Add redirect if user is logged in

--- sdr_financial/views.py
+++ sdr_financial/views.py
@@ -3,4 +3,6 @@ from django.contrib.auth import login, authenticate
 
 
 def home(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard')
     return render(request, "home.html", {})
