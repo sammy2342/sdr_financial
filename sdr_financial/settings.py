3ee786bb6c6d0575f125d97911a68f429ada5021
ricardo39985 <ricardo39985@gmail.com>
2022-10-31 13:40:45 -0400

    update login ur

--- sdr_financial/settings.py
+++ sdr_financial/settings.py
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LOGIN_URL = '/login/'
-LOGIN_REDIRECT_URL = '/home/'
+LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = '/login/'
 
 # Internationalization
