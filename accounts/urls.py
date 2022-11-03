c574d32867c6ae3e8440578c828b118b8dbb8f5f
ricardo39985 <ricardo39985@gmail.com>
2022-11-03 13:52:48 -0400

    Add routes to handle profile update

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
+    # Get user profile update form
+    path('update_user', views.update_user, name='update_user'),
+    path('apply_user_update', views.apply_user_update, name='apply_update')
+
 ]
