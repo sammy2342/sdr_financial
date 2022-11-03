3a6d7e6e495af71f53e09da2d5c50a3406011706
ricardo39985 <ricardo39985@gmail.com>
2022-11-03 13:53:24 -0400

    Add imports, add fucntions for profile update

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+# Decoreator to ensure user is logged in
+from django.contrib.auth.decorators import login_required
+from .models import Profile
+from django.contrib import messages
+
 
 # Views
 
@@ -12,7 +17,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')
-            user = authenticate(username = username, password = password)
+            user = authenticate(username=username, password=password)
             login(request, user)
             return redirect('home')
     else:
@@ -20,3 +25,21 @@ def register(request):
     return render(request, 'registration/register.html', {'form': form})
 
 
+# Get form for user update
+@login_required
+def update_user(request):
+    return render(request, 'profiles/update.html')
+
+
+@login_required
+def apply_user_update(request):
+    profile = request.user.profile
+    profile.first_name = request.POST['first_name'] if request.POST['first_name'] else profile.first_name
+    profile.last_name = request.POST['last_name'] if request.POST['last_name'] else profile.last_name
+    profile.bio = request.POST['bio'] if request.POST['bio'] else profile.bio
+    profile.address = request.POST['address'] if request.POST['address'] else profile.address
+    profile.save()
+    messages.add_message(request, messages.INFO,
+                         'Your info has been saved')
+
+    return redirect('update_user')
